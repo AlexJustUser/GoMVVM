206037110594003b615d5477aefbb613534b2bbb
Aleks <swimmersuper16@gmail.com>
2021-05-14 18:31:28 +0300

    Add some logic. When app starts receiving data, when it closes - sending data with lifecycle

--- app/src/main/java/com/mvvmtest/MainActivity.kt
+++ app/src/main/java/com/mvvmtest/MainActivity.kt
@@ -10,34 +10,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println(object : Any(){}.javaClass.enclosingMethod?.name)
+        lifecycle.addObserver(getData)
+        //println(object : Any(){}.javaClass.enclosingMethod?.name)
     }
 
-    override fun onStart() {
-        println(object : Any(){}.javaClass.enclosingMethod?.name)
-        getData.getData()
-        super.onStart()
-    }
-
-    override fun onResume() {
-        println(object : Any(){}.javaClass.enclosingMethod?.name)
-        super.onResume()
-    }
-
-    override fun onPause() {
-        println(object : Any(){}.javaClass.enclosingMethod?.name)
-        super.onPause()
-    }
-
-    override fun onStop() {
-        println(object : Any(){}.javaClass.enclosingMethod?.name)
-        getData.sendData()
-        super.onStop()
-    }
-
-    override fun onDestroy() {
-        println(object : Any(){}.javaClass.enclosingMethod?.name)
-        super.onDestroy()
-    }
+//    override fun onStart() {
+//        println(object : Any(){}.javaClass.enclosingMethod?.name)
+//        getData.getData()
+//        super.onStart()
+//    }
+//
+//    override fun onResume() {
+//        println(object : Any(){}.javaClass.enclosingMethod?.name)
+//        super.onResume()
+//    }
+//
+//    override fun onPause() {
+//        println(object : Any(){}.javaClass.enclosingMethod?.name)
+//        super.onPause()
+//    }
+//
+//    override fun onStop() {
+//        println(object : Any(){}.javaClass.enclosingMethod?.name)
+//        getData.sendData()
+//        super.onStop()
+//    }
+//
+//    override fun onDestroy() {
+//        println(object : Any(){}.javaClass.enclosingMethod?.name)
+//        super.onDestroy()
+//    }
 
 }
