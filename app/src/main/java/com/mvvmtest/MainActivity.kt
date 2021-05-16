67b200d0643ca6cc077876a32e852bcd55449adf
Aleks <swimmersuper16@gmail.com>
2021-05-16 18:32:31 +0300

    Add Navigation

--- app/src/main/java/com/mvvmtest/MainActivity.kt
+++ app/src/main/java/com/mvvmtest/MainActivity.kt
@@ -3,6 +3,8 @@ package com.mvvmtest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.lifecycle.*
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -10,11 +12,13 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var navController: NavController
     lateinit var  mViewModel : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this, R.id.nav_host)
         mViewModel = ViewModelProvider(this, MainFactory(application, "Factory")).get(MainViewModel::class.java)
     }
 
